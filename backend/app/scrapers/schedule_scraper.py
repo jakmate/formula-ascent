e75c5a21fdf1baee27694ef81adceca4093d11c6
jakmate <jakub.orzolek04@gmail.com>
2025-11-06 20:26:49 +0000

    rm print statements oops

--- backend/app/scrapers/schedule_scraper.py
+++ backend/app/scrapers/schedule_scraper.py
@@ -177,9 +177,7 @@ def parse_time_to_datetime(time_str, base_date, day_name=None, location=None):
         # Convert to UTC if location is provided
         if location:
             tz_str = get_timezone_for_location(location)
-            print(tz_str)
             tz = pytz.timezone(tz_str)
-            print(tz)
 
             # Localize and convert to UTC
             start_dt = tz.localize(start_dt).astimezone(pytz.utc)
